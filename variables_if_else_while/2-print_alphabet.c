00bba8c07aad183516e7ffb845fa64bbe3d39428
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-22 12:42:46 +0200

    test

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,18 @@
 /* more headers goes there */
 
 /**
-* main - 
+* main - print the hole alphabet
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-    for (c = 'a';c < 'z';)
-	    return (0);
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
