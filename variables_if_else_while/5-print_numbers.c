560b07ea39633588506a1e59d90a203c9dc2b3c7
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-22 19:29:44 +0200

    commit

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int n = 0;
-
 	while (n < 10)
 	{
 		printf("%d", n);
--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,7 +10,13 @@
 */
 int main(void)
 {
-	
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
 	return (0);
 }
-		
