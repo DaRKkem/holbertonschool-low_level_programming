e35cb06dfa0fbda36e1224900f7f794c7f8fbbcd
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-24 18:03:15 +0200

    commit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @x: 1st integer to add
+ * @y: 2nd integer to add
+ *
+ * Return: Always 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
