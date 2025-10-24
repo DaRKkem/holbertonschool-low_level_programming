fc35d14e5dee6ac757d6fe1104fb405f5ef237ba
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-24 11:08:10 +0200

    commit

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _islower - checks if a character is in scope [a-z]
@@ -8,12 +9,14 @@
  */
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
+		puts("1");
 		return (1);
 	}
 	else
 	{
+		puts("0");
 		return (0);
 	}
 }
