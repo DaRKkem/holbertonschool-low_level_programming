8f73641c9c86107ed2cedf5fe750e90a36d82909
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-24 18:16:15 +0200

    commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to enter
+ *
+ * Return: Always nothing
+ */
+void print_to_98(int n)
+{
+	if (n != 98)
+	{
+		printf("%d, ", n);
+	}
+	else
+	{
+		printf("98\n", n);
+	}
+
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("98\n");
+		}
+	}
+}
