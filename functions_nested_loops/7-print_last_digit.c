6300362b936d964a0994ef996bc301ea0f6fde11
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-24 15:40:48 +0200

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - prints the last digit of a number
@@ -8,6 +9,10 @@
  */
 int print_last_digit(int n)
 {
+	if (n < 0)
+	{
+		n -= 2*n;
+	}
 	n = n % 10;
 	_putchar(n + '0');
 	return (n);
