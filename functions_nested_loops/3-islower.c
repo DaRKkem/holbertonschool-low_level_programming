63ca3a1d093cb3b5689433ad8a395b2dead3830b
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-24 10:52:37 +0200

    commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is in scope [a-z]
+ *
+ *
+ * Return: 1 if 'c' is in lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		eturn (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
