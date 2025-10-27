3985f7d24bcf1faa48956b5aae21d3cc33278a4a
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-27 10:24:50 +0100

    commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: the caracter to verifie
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
