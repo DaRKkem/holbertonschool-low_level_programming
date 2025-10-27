8edfc8cb2565aee04a3209e37310a70fa0ae8de1
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-27 14:59:43 +0100

    commit

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_most_numbers - the numbers, from 0 to 9 without 2 & 4
+ * print_most_numbers - prints the numbers, from 0 to 9, without 2 & 4
  *
  * Return: Always 0.
  */
@@ -20,4 +20,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int n = 0;
+
+		while (n <= 14)
+		{
+			if(n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			n++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
