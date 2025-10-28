25a5764ce255989b910bc5c74d9a1291dcbb72d9
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-28 10:52:23 +0100

    commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,6 +6,7 @@
  *				prints Fizz for multiples of three,
  *				prints Buzz for multiples of five,
  *				prints FizzBuzz for multiples of three and five
+ * main -		main function, launches FizzBuzz function
  *
  * Return: Always 0.
  */
@@ -15,17 +16,17 @@ void FizzBuzz(void)
 
 	while (i <= 100)
 	{
-		if (i % 5 == 0)
+		if (i % 5 == 0 && i % 3 != 0)
 		{
-			if (i % 3 == 0)
-			{
-				printf("FizzBuzz ");
-			}
 			printf("Buzz ");
 		}
 		else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
 		}
 		else
 		{
