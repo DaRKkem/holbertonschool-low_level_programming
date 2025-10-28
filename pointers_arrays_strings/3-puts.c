13478bd9ad3de12c5cf84a520431a6e13ebcfa8c
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-28 14:31:54 +0100

    commit

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: the string
+ *
+ * Return: Always nothing.
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
