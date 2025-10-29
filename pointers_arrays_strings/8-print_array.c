8aa9bb225132e832f0fadf35b14bdbf9464e0922
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-10-29 12:32:50 +0100

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: the string
+ * @n: the number of int in 'a' to display starting from 1
+ *
+ * Return: Always nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+		if (i < n)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
