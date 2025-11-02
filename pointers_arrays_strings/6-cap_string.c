6d656df1bac5b5221857eec73aa7b0a34a48ecf8
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-02 21:36:16 +0100

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,38 +16,12 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i - 1] < 'a' || str[i - 1] > 'z')
+			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '\0' || str[i - 1] == '-')
 			{
-				if (str[i - 1] < 'A' || str[i - 1] > 'Z')
-				{
-					if (str[i - 1] < '0' || str[i - 1] > '9')
-					{
-						str[i] -= 'a' - 'A';
-					}
-				}
+				str[i] -= 'a' - 'A';
 			}
 		}
 		i++;
 	}
 	return (str);
-}
-
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-	str[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world";
-	ptr = cap_string(str);
-    printf("%s", ptr);
-	str[] = "HELLO WORLD! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world";
-	ptr = cap_string(str);
-    printf("%s", ptr);
-	str[] = "-- expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	ptr = cap_string(str);
-    printf("%s", ptr);
-
-    return (0);
 }
