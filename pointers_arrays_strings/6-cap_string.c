2d1f775a0785836185023faa1807ea6c1fc38599
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-02 21:58:07 +0100

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,9 +16,15 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if ((str[i - 1] < 'a' || str[i - 1] > 'z') && (str[i - 1] < 'A' || str[i - 1] > 'Z') && str[i - 1] != '-' && (str[i - 1] < '0' || str[i - 1] > '9'))
+			if (str[i - 1] < 'a' || str[i - 1] > 'z')
 			{
-				str[i] -= 'a' - 'A';
+				if (str[i - 1] < 'A' || str[i - 1] > 'Z')
+				{
+					if (str[i - 1] != '-' && (str[i - 1] < '0' || str[i - 1] > '9'))
+					{
+						str[i] -= 'a' - 'A';
+					}
+				}
 			}
 		}
 		i++;
