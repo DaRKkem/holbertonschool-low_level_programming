6277663a0b86e1b18f62428962fba84cacdbe6cf
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-02 20:17:47 +0100

    commit

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: the string
+ *
+ * Return: The new string.
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+	char *tab = *;
+	while (tab[i] != '\0')
+	{
+
+	}
+	return (*);
+}
+int main(void)
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
