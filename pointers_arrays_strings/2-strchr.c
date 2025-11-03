7c1fc6b6253fe49deee501f4121afac1982b4b21
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-03 13:59:58 +0100

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character to locate in 's'
+ *
+ * Return: The pointer of the first occurence or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
