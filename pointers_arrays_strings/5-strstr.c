a2dc578f58ef34e5d32930ce01b7719831e89828
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-04 16:14:07 +0100

    commit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,28 +4,28 @@
 
 /**
  * _strstr - locates a substring
- * @s: the string
- * @needle: the number of bytes
+ * @haystack: the string to check
+ * @needle: the string to check in 'haystack'
  *
- * Return: The number of bytes in the initial segment of s
+ * Return: The pointer of the first iteration of 'needle' in 'haystack'
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
-        if (needle[j] == '\0')
-            return (haystack + i);
-    }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
 
-    return (NULL);
+	return (NULL);
 }
