61b846d31215f6c8c11d74070051c2fdff972dd0
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-05 15:46:04 +0100

    commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,17 +13,17 @@ int helper(int n, int i)
 {
 	if (n <= 1)
 	{
-		return (0);
+		return (0);  // Si on test si 1 ou 0 est premier (ne le sont dans aucun cas donc return 0)
 	}
 	if (n % i == 0)
 	{
-		return (0);
+		return (0);  // Diviseur trouvé pour ce nombre donc PAS premier (donc return 0)
 	}
 	if (i * i > n)
 	{
-		return (1);
+		return (1);  // Si c'est un nombre permier (return 1)
 	}
-	return (helper(n, i + 1));
+	return (helper(n, i + 1)); // Teste le diviseur suivant
 }
 
 /**
@@ -34,6 +34,7 @@ int helper(int n, int i)
  */
 int is_prime_number(int n)
 {
-	return (helper(n, 2));
+	return (helper(n, 2)); // On commence à 2 car rien ne sedivise par 0 et 1 divise tous les nombres connus.
+						   //Donc on commence par vérifier si le nombre est divisible par 2 d'abord.
 	return (0);
 }
