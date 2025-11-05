1ea1f1c2e66f0cf178cd759bf05fe16303f6789a
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-05 12:39:33 +0100

    commit

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _strlen_recursion - returns the length of a string by using recursion
+ * @s: the string to use in recursion
+ *
+ * Return: Always nothing.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+}
