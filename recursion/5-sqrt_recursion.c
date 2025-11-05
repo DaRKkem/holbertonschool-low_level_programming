8a27e43ea85c3b9d6f5b7d38b8ab70c82eb254e5
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-05 15:09:29 +0100

    commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,20 +8,16 @@
  *
  * Return: The square root of 'n' or -1
  */
+static int helper(int n, int i)
+{
+	if (i * i == n)
+		return i;
+	if (i * i > n)
+		return -1;
+	return helper(n, i + 1);
+}
+
 int _sqrt_recursion(int n)
 {
-	int helper(int n, int i)
-	{
-		if (i * i == n)
-		{
-			return i;
-		}
-		if (i * i > n)
-		{
-			return -1;
-		}
-		return helper(n, i + 1);
-	}
-
 	return helper(n, 0);
 }
