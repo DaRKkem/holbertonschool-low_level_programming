9425d48d802fe88f15cbbf61f039f0ca8ae19870
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-05 15:08:00 +0100

    commit

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @x: the number to find the square root
+ *
+ * Return: The square root of 'n' or -1
+ */
+int _sqrt_recursion(int n)
+{
+	int helper(int n, int i)
+	{
+		if (i * i == n)
+		{
+			return i;
+		}
+		if (i * i > n)
+		{
+			return -1;
+		}
+		return helper(n, i + 1);
+	}
+
+	return helper(n, 0);
+}
