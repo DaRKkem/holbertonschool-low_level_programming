d99fbe6e3ce3c50f469e6ffb67faac96175e53ca
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-07 19:09:42 +0100

    commit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		/* check if each argument contains only digits */
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit((unsigned char)argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
