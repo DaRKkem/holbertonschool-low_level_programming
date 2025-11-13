1b98b5bbeb669a662781aad29d4c2cfe1b4986e9
root <root@LAPTOP-KEJJSN8M.localdomain>
2025-11-13 09:58:44 +0100

    commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,38 +3,41 @@
 #include <unistd.h>
 
 /**
- * helper - returns 1 if is a prime number, else returns 0
- * @n: the number to test if is prime or not
- * @i: the index
- *
- * Return: 1 or 0
- */
-int helper(int n, int i)
-{
-	if (n <= 1)
-	{
-		return (0);  // Si on test si 1 ou 0 est premier (ne le sont dans aucun cas donc return 0)
-	}
-	if (n % i == 0)
-	{
-		return (0);  // Diviseur trouvé pour ce nombre donc PAS premier (donc return 0)
-	}
-	if (i * i > n)
-	{
-		return (1);  // Si c'est un nombre permier (return 1)
-	}
-	return (helper(n, i + 1)); // Teste le diviseur suivant
-}
-
-/**
  * is_prime_number - returns 1 if is a prime number, else returns 0
  * @n: the number to test if is prime or not
  *
  * Return: 1 or 0
  */
-int is_prime_number(int n)
+int is_prime_number(int n, int i)
 {
-	return (helper(n, 2)); // On commence à 2 car rien ne sedivise par 0 et 1 divise tous les nombres connus.
-						   //Donc on commence par vérifier si le nombre est divisible par 2 d'abord.
-	return (0);
+	if (n < 2)
+		return (0);  // Si on test si 1 ou 0 est premier (ne le sont dans aucun cas donc return 0)
+	if (n % i == 0)
+		return (0);  // Diviseur trouvé pour ce nombre donc PAS premier (donc return 0)
+	if (i * i > n)
+		return (1);  // Si c'est un nombre permier (return 1)
+	return (is_prime_number(n, i + 1)); // Teste le diviseur suivant
+}
+
+int main(void)
+{
+    int r;
+
+    r = is_prime_number(1, 2);
+    printf("%d\n", r);
+    r = is_prime_number(1024, 2);
+    printf("%d\n", r);
+    r = is_prime_number(16, 2);
+    printf("%d\n", r);
+    r = is_prime_number(17, 2);
+    printf("%d\n", r);
+    r = is_prime_number(25, 2);
+    printf("%d\n", r);
+    r = is_prime_number(-1, 2);
+    printf("%d\n", r);
+    r = is_prime_number(113, 2);
+    printf("%d\n", r);
+    r = is_prime_number(7919, 2);
+    printf("%d\n", r);
+    return (0);
 }
