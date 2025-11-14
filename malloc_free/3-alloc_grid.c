a8438ae28ac7bdf54a54f9e69d378bf1a5f7d4b7
DaRKkem <damross446@gmail.com>
2025-11-14 22:16:53 +0100

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,10 +13,31 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int *arr;
+	int i, j;
+	int **arr;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	
+	arr = malloc(height * sizeof(int *));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i]= malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
+		{
+*
+
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			arr[i][j];
+	}
 
 	return (arr);
 }
