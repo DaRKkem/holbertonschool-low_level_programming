723ed0c1ef058513c5b1036b2ae68b8684559fb6
DaRKkem <damross446@gmail.com>
2025-11-14 22:37:07 +0100

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <stdlib.h>
 /**
@@ -23,29 +22,3 @@ void free_grid(int **grid, int height)
 
 	free(grid);
 }
-=======
-#include "main.h"
-#include <stdlib.h>
-/**
-* free_grid - frees a 2 dimensional grid previously
-* @grid: double pointeur used
-* @height: height of the array
-*
-* Return: Always nothing.
-*/
-
-void free_grid(int **grid, int height)
-{
-	int i;
-
-	if (grid == NULL)
-		return;
-
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
-	}
-
-	free(grid);
-}
->>>>>>> 7df7ec282d8ebad6db57e29b871c1b89838d566f
