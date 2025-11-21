4a6d840316e58d97988d340cb94a4e1866ff80bf
DaRKkem <damross446@gmail.com>
2025-11-21 14:41:06 +0100

    commit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - prints numbers, followed by a new line
+* @separator: to separate arguments by a coma
+* @n: number of arguments
+*
+* Return: Always nothing.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+	int stock;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		stock = va_arg(args, int);
+
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+
+		printf("%d", stock);
+	}
+	va_end(args);
+	printf("\n");
+}
