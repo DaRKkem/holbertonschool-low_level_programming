a39f54a3b493909a328c8a016688431308499969
DaRKkem <damross446@gmail.com>
2025-11-21 14:52:02 +0100

    commit

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,43 +14,48 @@ void print_all(const char * const format, ...)
 {
 	va_list boite;
 	int i = 0;
-	int check = 1;
+	int printed = 0;  /* becomes 1 after first successful print */
 	char *stock;
 
 	va_start(boite, format);
 
-	if (format == NULL)
-		{
-			printf("\n");
-			va_end(boite);
-			return;
-		}
-	while (format[i] != '\0')
+	while (format && format[i])
 	{
-		if (check == 0)
-			printf(", ");
-		check = 0;
 		switch (format[i])
 		{
 		case 'c':
+			if (printed)
+				printf(", ");
 			printf("%c", va_arg(boite, int));
+			printed = 1;
 			break;
-		case 'i':
-			printf("%d", va_arg(boite, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(boite, double));
-			break;
-		case 's':
-				stock = va_arg(boite, char *);
 
-				printf("%s", stock ? stock : "(nil)");
-				break;
+		case 'i':
+			if (printed)
+				printf(", ");
+			printf("%d", va_arg(boite, int));
+			printed = 1;
+			break;
+
+		case 'f':
+			if (printed)
+				printf(", ");
+			printf("%f", va_arg(boite, double));
+			printed = 1;
+			break;
+
+		case 's':
+			if (printed)
+				printf(", ");
+			stock = va_arg(boite, char *);
+			printf("%s", stock ? stock : "(nil)");
+			printed = 1;
+			break;
 		}
+
 		i++;
 	}
 
 	va_end(boite);
-
 	printf("\n");
 }
