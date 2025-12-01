c418bb0b02f2eedd2ced39a2113cef1d2bf43888
DaRKkem <damross446@gmail.com>
2025-12-01 15:21:39 +0100

    commit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,15 +11,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		
-		{
-			if (h->str == NULL)
-				puts("[0] (nil)");
-		}
+		if (h->str == NULL)
+			puts("[0] (nil)");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		count++;
 	}
