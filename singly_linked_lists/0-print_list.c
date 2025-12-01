f45c7222620f2bf852ae44535546ffcb85f3aec6
DaRKkem <damross446@gmail.com>
2025-12-01 13:32:58 +0100

    commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	if (h == NULL)
+	{
+		puts("[0] (nil)");
+		return (0);
+	}
+
+	while (h->next != NULL)
+	{
+		printf("%s", h->str);
+		h = h->next;
+		count++;
+	}
+
+	return(count);
+}
