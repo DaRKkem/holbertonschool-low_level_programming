69595cbed0697c147be4f5c7ee810ab9a25f3397
DaRKkem <damross446@gmail.com>
2025-12-04 10:54:38 +0100

    commit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,27 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to the pointer to the first node of the list
- * @str: the string to duplicate and store in the new node
+ * @str: string to duplicate and store in the new node
  *
- * Return: address of the new element or NULL
+ * Return: address of the new element or NULL on failure
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *tmp;
 	char *copy;
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
-	if (str == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	copy = strdup(str);
 	if (copy == NULL)
 		return (NULL);
+
+	while (copy[i])
+		i++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -30,53 +32,23 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	while (copy[i])
-		i++;
-
-	while (head->next)
-		head = head->next;
-
 	new_node->str = copy;
 	new_node->len = i;
 	new_node->next = NULL;
-	head->next = new_node;
+
+	/* Cas : liste vide */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Sinon, parcourir jusqu'au dernier élément */
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
 
 	return (new_node);
-}
-
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Anne");
-    add_node_end(&head, "Colton");
-    add_node_end(&head, "Corbin");
-    add_node_end(&head, "Daniel");
-    add_node_end(&head, "Danton");
-    add_node_end(&head, "David");
-    add_node_end(&head, "Gary");
-    add_node_end(&head, "Holden");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "Jimmy");
-    add_node_end(&head, "Justin");
-    add_node_end(&head, "Kalson");
-    add_node_end(&head, "Kina");
-    add_node_end(&head, "Matthew");
-    add_node_end(&head, "Max");
-    add_node_end(&head, "Michael");
-    add_node_end(&head, "Ntuj");
-    add_node_end(&head, "Philip");
-    add_node_end(&head, "Richard");
-    add_node_end(&head, "Samantha");
-    add_node_end(&head, "Stuart");
-    add_node_end(&head, "Swati");
-    add_node_end(&head, "Timothy");
-    add_node_end(&head, "Victor");
-    add_node_end(&head, "Walton");
-    print_list(head);
-    return (0);
 }
