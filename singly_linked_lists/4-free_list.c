033d8f0d9202a8bfbea1686c0f3b480fe7ad66db
DaRKkem <damross446@gmail.com>
2025-12-04 11:51:46 +0100

    commit

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
+ *
+ * Return: Always nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+
+	return;
+}
