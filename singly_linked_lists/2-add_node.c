3bf3cdc0a2664522b56e08d44c84ac7b34db3564
DaRKkem <damross446@gmail.com>
2025-12-04 09:41:47 +0100

    commit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,65 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * list_len - adds a new node at the beginning of a list_t list
- * @head: first node of the list
- * @str: the string
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer to the first node of the list
+ * @str: the string to duplicate and store in the new node
  *
- * Return: address of the new element, or NULL
+ * Return: address of the new element, or NULL on failure
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t new_list;
-	char *copy;
+    list_t *new_node;
+    char *copy;
 
-	while (head != NULL)
-	{
-		str = head->str;
-		*head->str = copy;
-		if (*head->next == NULL)
-		{
-			printf("OUI");
-		}
-		*head = head->next;
-	}
+    if (str == NULL)
+        return (NULL);
 
-	free(copy);
-	return (*head);
-}
+    copy = strdup(str);
+    if (copy == NULL)
+        return (NULL);
 
-int main(void)
-{
-    list_t *head;
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+    {
+        free(copy);
+        return (NULL);
+    }
 
-    head = NULL;
-    add_node(&head, "Alexandro");
-    add_node(&head, "Asaia");
-    add_node(&head, "Augustin");
-    add_node(&head, "Bennett");
-    add_node(&head, "Bilal");
-    add_node(&head, "Chandler");
-    add_node(&head, "Damian");
-    add_node(&head, "Daniel");
-    add_node(&head, "Dora");
-    add_node(&head, "Electra");
-    add_node(&head, "Gloria");
-    add_node(&head, "Joe");
-    add_node(&head, "John");
-    add_node(&head, "John");
-    add_node(&head, "Josquin");
-    add_node(&head, "Kris");
-    add_node(&head, "Marine");
-    add_node(&head, "Mason");
-    add_node(&head, "Praylin");
-    add_node(&head, "Rick");
-    add_node(&head, "Rick");
-    add_node(&head, "Rona");
-    add_node(&head, "Siphan");
-    add_node(&head, "Sravanthi");
-    add_node(&head, "Steven");
-    add_node(&head, "Tasneem");
-    add_node(&head, "William");
-    add_node(&head, "Zee");
-    print_list(head);
-    return (0);
+    new_node->str = copy;
+    new_node->len = strlen(copy);
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
 }
