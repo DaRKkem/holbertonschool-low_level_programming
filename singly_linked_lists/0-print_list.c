850828ac6e26b8c8517cbcfbd7b08f51e331f7b7
DaRKkem <damross446@gmail.com>
2025-12-04 09:35:06 +0100

    commit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,9 +3,9 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
+ * @h: first node of the list
  *
- * Return: the number of nodes
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
