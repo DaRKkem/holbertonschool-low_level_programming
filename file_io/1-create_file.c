15d19d8ce7bd4fe0a1b244488029a57f6e6b844f
DaRKkem <damross446@gmail.com>
2025-12-06 00:16:11 +0100

    commit

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with rw------- permissions and writes content
+ * @filename: Name of the file to create
+ * @text_content: NULL terminated string to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+	unsigned int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[len])
+		len++;
+
+	written = write(fd, text_content, len);
+	if (written == -1 || (unsigned int)written != len)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
